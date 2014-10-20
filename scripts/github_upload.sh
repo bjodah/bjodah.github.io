5159e163b4dfee7d6637d8675f387b3cef90474b
Björn Dahlgren <bjodah@gmail.com>
2014-10-20 13:44:27 +0200

    tweak upload script

--- scripts/github_upload.sh
+++ scripts/github_upload.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-OUTPUTDIR=$1
+#
+# Careful - this script rebases and pushes forcefully!
+#
+
+UPLOAD_DIR=$1
+OVERWRITE_UPLOAD_BRANCH=master
 WORKDIR=`pwd`
 TMPDIR=`mkdtemp -d`
 GITHUB_USER=bjodah
@@ -9,12 +14,10 @@ git config --global user.name "drone"
 git config --global user.email "drone@hera.physchem.kth.se"
 git clone --quiet git@github.com:/${GITHUB_USER}/${GITHUB_REPO} > /dev/null
 cd ${GITHUB_REPO}
-git branch -D master
-git checkout --orphan master
+git branch -D $OVERWRITE_UPLOAD_BRANCH
+git checkout --orphan $OVERWRITE_UPLOAD_BRANCH
 git rm -rf . > /dev/null
-cp -R ${WORKDIR}/skeleton/* .
-cp ${WORKDIR}/skeleton/.* .
-cp -r ${OUTPUTDIR} .
+cp -r ${UPLOAD_DIR}/* .
 git add -f . > /dev/null
 git commit -m "Lastest docs from successful drone build (hash: ${DRONE_COMMIT})"
-git push -f origin master
+git push -f origin $OVERWRITE_UPLOAD_BRANCH
